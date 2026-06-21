586a04ee301b956868efb5bc3b89fba78050f39f
Daffdhot29 <daff.yunus05@gmail.com>
2026-06-21 17:45:03 +0700

    push trial

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from paddleocr import PaddleOCR
 
 app = FastAPI(title="ALPR API")
 
-MODEL_PATH = "models/vehicle_plate_yolov9tiny_best.onnx"
+MODEL_PATH = "koushik-ai/yolov8-license-plate-detection/best.pt"
 
 UPLOAD_DIR = "uploads"
 OUTPUT_DIR = "outputs"
